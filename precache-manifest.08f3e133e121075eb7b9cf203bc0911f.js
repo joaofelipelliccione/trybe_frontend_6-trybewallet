9b9764ffe7e7e563d2e45bcd7b3a17d0a5445248
João Felipe P <jf.pelliccione@gmail.com>
2021-11-22 16:35:37 -0300

    Updates

---
+++ precache-manifest.08f3e133e121075eb7b9cf203bc0911f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd35e954aa780f906cd5602f74ebb77",
+    "url": "/trybe_frontend_6-trybewallet/index.html"
+  },
+  {
+    "revision": "29db37301e0804928ad8",
+    "url": "/trybe_frontend_6-trybewallet/static/css/main.a9df8846.chunk.css"
+  },
+  {
+    "revision": "48d791d95eb3e91e7c51",
+    "url": "/trybe_frontend_6-trybewallet/static/js/2.4f4ff944.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe_frontend_6-trybewallet/static/js/2.4f4ff944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29db37301e0804928ad8",
+    "url": "/trybe_frontend_6-trybewallet/static/js/main.0f212822.chunk.js"
+  },
+  {
+    "revision": "ae40fee7fa636c924e98",
+    "url": "/trybe_frontend_6-trybewallet/static/js/runtime-main.fb9da59b.js"
+  }
+]);
